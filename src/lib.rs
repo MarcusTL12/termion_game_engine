b6425d60c5d0aef06399bc47e4218d6874a591a5
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-02-12 18:56:24 +0100

    _

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,6 @@ use termion::{
     screen::AlternateScreen,
 };
 
-pub fn dostuff() {
-    println!("Heisann terminalting");
-}
-
 pub fn col2fg_str<T: color::Color>(col: T) -> Vec<u8> {
     let mut ret = Vec::new();
     write!(ret, "{}", color::Fg(col)).unwrap();
